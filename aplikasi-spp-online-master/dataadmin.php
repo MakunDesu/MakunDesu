eaafbfc95ab6cd4febe7ddba45fa4206da0075ae
MakunDesu <83714900+MakunDesu@users.noreply.github.com>
2021-05-05 21:59:09 +0700

    Add files via upload

---
+++ aplikasi-spp-online-master/dataadmin.php
@@ -0,0 +1,44 @@
+<?php include 'header.php'; ?>
+<style >
+	.btn{
+		margin-bottom: 10px;
+	}
+	body{
+    		background-color: white;
+    }
+</style>
+<div class="container">
+	<div class="page-header">
+<h2> DATA ADMIN SPP</h2>
+	</div>
+<a class="btn btn-primary " href="tambahAD.php">TAMBAH DATA</a>
+<?php
+	?>
+<table class="table table-bordered table-striped">
+ 	<tr>
+ 		<th>No</th>
+ 		<th>Id</th>
+ 		<th>Nama Admin</th>
+		<th>Lanjutan</th>
+ 	</tr>
+ 	<?php 
+ 	include 'koneksi.php';
+	$data = mysqli_query($konek,"SELECT * FROM admin ORDER BY idadmin ASC");	
+ 	$i=1; 
+ 	while($dta = mysqli_fetch_assoc($data) ):
+ 	 ?>
+ 	 <tr>
+ 	 	<td width="40px" align="center"><?= $i; ?></td>
+ 	 	<td align="center"><?= $dta['idadmin'] ?></td>
+ 	 	<td><?= $dta['namalengkap'] ?></td>
+ 	 	<td width="160px">
+ 	 		<a class="btn btn-warning btn-sm" href="updateAD.php?id=<?= $dta['idadmin'] ?>">Edit</a> 
+ 	 		<a class="btn btn-danger btn-sm" href="hapusAD.php?id=<?= $dta['idadmin'] ?>" onclick ="return confirm('apakah anda yakin ingin menghapus data admin? ')">Hapus</a>
+ 	 	</td>
+ 	 </tr>
+ 	 <?php $i++;  ?>
+ 	<?php endwhile; ?>
+ </table>
+</body>
+</div>
+</html>
---
+++ aplikasi-spp-online-master/index.php
@@ -0,0 +1,26 @@
+<?php include 'header.php'; ?>
+	<div class="container">
+		<div class="panel-body">
+	<h2>SELAMAT DATANG DI</h2>
+	<h3>Pendataan SPP</h3>
+	<h2>SMP NEGERI 3 Loksmewew</h2>
+	<style >
+    	body{
+    		background-image:url('img/images2.jpeg');
+    		background-size: cover;
+    	}
+		h2{
+			text-align: center;
+			text-shadow: 2px 3px 1px white;
+		}
+		h3{
+			text-align: center;
+			text-shadow: 2px 3px 1px white;
+		}
+		h2{
+			text-align: center;
+			text-shadow: 2px 3px 1px white;
+		}
+    </style>
+</div>
+</div>
